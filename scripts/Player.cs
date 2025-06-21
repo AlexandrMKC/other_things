3e54b86be1b61557c5d6cae4af61de0f43b5e21f
Alexander <alecsandr-k-18@mail.ru>
2025-06-21 21:44:49 +0300

    initial commit

---
+++ scripts/Player.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	public float speedMovement { get; set; } = 10.0F;
+
+	[Export]
+	public float speedRotation { get; set; } = 5.0F;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		_MouseRotation(delta);
+	
+		Vector2 mousePosition = GetGlobalMousePosition();
+		Vector2 toMouse = (mousePosition - GlobalPosition).Normalized();
+		Vector2 perpendicular = new Vector2(-toMouse.Y, toMouse.X);
+		float moveForward = Input.GetActionStrength("move_forward") - Input.GetActionStrength("move_backward");
+		float strafe = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+		Vector2 movement = toMouse * moveForward * speedMovement + perpendicular * strafe * speedMovement;
+		Velocity = movement;
+		MoveAndSlide();
+	}
+
+	private Vector2 Vector2(double v1, double v2)
+	{
+		throw new NotImplementedException();
+	}
+
+	private void _MouseRotation(double delta)
+	{
+		Vector2 mousePos = GetGlobalMousePosition();
+		float targetAngle = (mousePos - GlobalPosition).Angle();
+		Rotation = Mathf.LerpAngle(Rotation, targetAngle,speedRotation*(float)delta);
+	}
+}
